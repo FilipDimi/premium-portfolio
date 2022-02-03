6d27ebeaa28e645f02bbe5e9afbbb5cc74f9cbd7
Filip Dimitrievski <filipdimitrievski@protonmail.com>
2022-02-03 16:51:43 -0500

    URLS connected to the dedicated views

--- premiumportfolio/core/views.py
+++ premiumportfolio/core/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Core pages views
+def home(request):
+    """Send GET request for the index page"""
+
+    passing_dict = {}
+    return render(request, 'core/index.html', passing_dict)
+
+
+def about(request):
+    """Send GET request for the about page"""
+
+    passing_dict = {}
+    return render(request, 'core/about.html', passing_dict)
+
+
+def services(request):
+    """Send GET request for the services page"""
+
+    passing_dict = {}
+    return render(request, 'core/services.html', passing_dict)
+
+
+def works(request):
+    """Send GET request for the works page"""
+
+    passing_dict = {}
+    return render(request, 'core/works.html', passing_dict)
+
+
+def contact(request):
+    """Send GET request for the contact page"""
+
+    passing_dict = {}
+    return render(request, 'core/contact.html', passing_dict)
